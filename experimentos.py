d114ed337a9377baeb3e17889d90103f2dff45df
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2024-03-28 10:27:58 -0300

    Add files via upload

--- experimentos.py
+++ experimentos.py
@@ -3,7 +3,7 @@ import networkx as nx
 
 G = functions.graph_network(year = None, all = True)
 print(f"Grafo com {G.number_of_edges()} arestas e {G.number_of_nodes()} vértices")
-nx.write_gml(G, "Grafo_Clique.gml")
+nx.write_gml(G, "grafo_clique.gml")
 
 functions.centralidade_G(G, "Resultados")
 functions.comunidades_G(G)
@@ -14,7 +14,7 @@ print(f"Hipergrafo com {HG.number_of_edges()} arestas e {HG.number_of_nodes()} v
 for x in range(1,4):
     linegraph = HG.get_linegraph(s=x, edges=False)
     print(f"Linegraph com {linegraph.number_of_edges()} arestas e {linegraph.number_of_nodes()} vértices")
-    
+
     functions.degree_centrality_hg(HG, "Resultados", x)
     functions.centralidade_HG(linegraph, "Resultados", x)
 
